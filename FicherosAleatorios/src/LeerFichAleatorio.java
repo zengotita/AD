fde8bf2c510f7c576e07d11adc283ccaa389530b
Jokin Zengotitabengoa <jzengotitabengoa@egibide.org>
2022-09-23 19:01:24 +0200

    Reorganizacion ficheros aleatorios

--- FicherosAleatorios/src/LeerFichAleatorio.java
+++ FicherosAleatorios/src/LeerFichAleatorio.java
@@ -3,7 +3,7 @@ import java.io.*;
 public class LeerFichAleatorio {
     public static void main (String[] args) throws IOException {
 
-        File fichero = new File(".//ficheros//AleatorioEmple.dat");
+        File fichero = new File(".\\ficheros\\AleatorioEmple.dat");
         //Declaramos el fichero de acceso aleatorio
         RandomAccessFile file = new RandomAccessFile(fichero, "r");
 
